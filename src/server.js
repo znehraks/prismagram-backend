e0aedd8deeb69b812743a522098b960471449b2e
DesignC <znehraks@gmail.com>
2019-11-22 02:05:01 +0900

    add package json engines

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 import "./env";
 import{GraphQLServer} from "graphql-yoga";
-import {prisma} from "../generated/prisma-client"
 import logger from "morgan";
-import passport from "passport";
 import schema from "./schema";
 import "./passport";
 import {authenticateJwt} from "./passport";
