8677166673fc4cfddce92532a2e66e36b9f95c2f
DesignC <znehraks@gmail.com>
2019-11-22 02:15:10 +0900

    server config

--- src/server.js
+++ src/server.js
@@ -1,24 +1,23 @@
 import "./env";
-import{GraphQLServer} from "graphql-yoga";
+import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
 import "./passport";
-import {authenticateJwt} from "./passport";
-import { isAuthenticated } from "./middlewears";
+import { authenticateJwt } from "./passport";
+import { isAuthenticated } from "./middlewares";
 import { uploadMiddleware, uploadController } from "./upload";
 
-
-const PORT = process.env.PORT || 4000; 
+const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({
-    schema, 
-    context: ({request}) => ({request, isAuthenticated})
+  schema,
+  context: ({ request }) => ({ request, isAuthenticated })
 });
 
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
 server.express.post("/api/upload", uploadMiddleware, uploadController);
 
-server.start({port: PORT}, () => 
-    console.log(`Server running on http://localhost:${PORT}`)
+server.start({ port: PORT }, () =>
+  console.log(`✅ Server running on http://localhost:${PORT}`)
 );
