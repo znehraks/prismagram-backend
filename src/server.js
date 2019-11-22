07528eeb393cc97f1d1d8035d119bfb3546b54f0
DesignC <znehraks@gmail.com>
2019-11-22 19:37:47 +0900

    plz..

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import { authenticateJwt } from "./passport";
 import { isAuthenticated } from "./middlewears";
 import { uploadMiddleware, uploadController } from "./upload";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000; 
 
 const server = new GraphQLServer({
   schema,
