4f335eadb75c5fa6c122da38f740f2f08233e4a7
DesignC <znehraks@gmail.com>
2019-11-23 02:36:47 +0900

    try again

--- src/utils.js
+++ src/utils.js
@@ -1,32 +1,9 @@
 import { adjectives, nouns } from "./words";
-import nodemailer from "nodemailer";
-import sgTransport from "nodemailer-sendgrid-transport";
 import jwt from "jsonwebtoken";
 
 export const generateSecret = () => {
     const randomNumber = Math.floor(Math.random() * adjectives.length);
     return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
-};
-
-export const sendMail = (email) => {
-    const options = {
-        auth: {
-            api_user: process.env.SENDGRID_USERNAME,
-            api_key: process.env.SENDGRID_PASSWORD
-        }
-    };
-    const client = nodemailer.createTransport(sgTransport(options));
-    return client.sendMail(email);
-};
-
-export const sendSecretMail = (address, secret) => {
-    const email = {
-        from: "znehraks@prismagram.com",
-        to: address,
-        subject: "Login Secret for Prismagram",
-        html: `Hello! Your login secret is <strong>${secret}</strong>. <br/>Copy paste on the app to login in`
-    };
-    return sendMail(email);
 };
 
 export const generateToken = (id) => jwt.sign({id}, process.env.JWT_SECRET);
